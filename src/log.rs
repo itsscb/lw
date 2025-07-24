7a8d6e0e74769331f16127e20732c50bd57d8af2
itsscb <dev@itsscb.de>
2025-07-24 11:53:53 +0200

    feat: add uuid to logs

--- src/log.rs
+++ src/log.rs
@@ -1,8 +1,10 @@
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Item {
+    id: Uuid,
     content: String,
     created: DateTime<Local>,
     modified: DateTime<Local>,
@@ -17,12 +19,17 @@ impl Item {
         self.content = content;
         self.modified = Local::now();
     }
+
+    pub fn id(&self) -> String {
+        self.id.to_string()
+    }
 }
 
 impl Default for Item {
     fn default() -> Self {
         let now = Local::now();
         Self {
+            id: Uuid::new_v4(),
             content: String::new(),
             created: now,
             modified: now,
